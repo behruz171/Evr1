a5c3aba88afeff15e49fefbaa53aa72a266de1fd
behruz171 <behruzzo662@gmail.com>
2023-08-09 13:38:18 +0500

    nimadir

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,11 @@ import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { Link, Outlet } from 'react-router-dom'
 import logo from '../img/logo.png'
 import profile from '../img/profile.png'
-
+function reload(){
+    setTimeout(() => {
+        window.location.reload()
+    }, 1);
+}
 const Navbar = () => {
     return (
         <nav>
@@ -14,14 +18,18 @@ const Navbar = () => {
             </label>
             <img src={logo} alt="" />
             <ul>
-                <Link to='/'>
+                <Link to='/' onClick={reload}>
                     <li><a className="active" href="#">Asosiy sahifa</a></li>
                 </Link>
-                <Link to='/batafsil'>
+                <Link to='/batafsil' onClick={reload}>
                     <li><a href="#">Biz haqimizda</a></li>
                 </Link>
+                <Link to='/' onClick={reload}>
                 <li><a href="#">Kurslar</a></li>
+                </Link>
+                <Link to='/' onClick={reload}>
                 <li><a href="#">Bog'lanish</a></li>
+                </Link>
                 <li><select className="tillar">
                     <option value="">uz</option>
                     <option value="">ru</option>
